3798ec38cb2e5064b4c99620e1de9a3915ddfd6f
sebastiao ferreira <sebastiao553@gmail.com>
2026-05-22 17:01:27 -0300

    chore: untrack build artifacts and claude config

--- app/.next-user/static/chunks/app/(auth)/forgot-password/page-d922bf974dd9b313.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5890],{9917:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,2619,23))}},e=>{e.O(0,[2619,8441,1255,7358],()=>e(e.s=9917)),_N_E=e.O()}]);
